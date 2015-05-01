f17f72729f4861a5026e1b0e01795af3c8368e89
donnell74 <donnell74@live.missouristate.edu>
2015-04-30 22:24:38 -0500

    Final commit with color and distance

--- app/src/main/java/com/example/id/moevents/MapFragment.java
+++ app/src/main/java/com/example/id/moevents/MapFragment.java
@@ -2,12 +2,14 @@ package com.example.id.moevents; // change to your own package
 
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,6 +22,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 /**
@@ -31,12 +36,18 @@ public class MapFragment extends Fragment {
     private GoogleMap googleMap;
     private Geocoder geocoder;
     public static CalendarEvent event = null;
+    double latitude = 37.267510;
+    double longitude = -93.169596;
+    private TextView mTravelTv;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // inflate and return the layout
         View v = inflater.inflate(R.layout.activity_map_fragment, container,
                 false);
+
+        mTravelTv = (TextView) getActivity().findViewById(R.id.TravelText);
 
         geocoder = new Geocoder(getActivity());
         mMapView = (SupportMapFragment) getChildFragmentManager()
@@ -53,6 +64,7 @@ public class MapFragment extends Fragment {
 
 
         googleMap = mMapView.getMap();
+        googleMap.setMyLocationEnabled(true);
         // latitude and longitude
         // mostate univ 37.199354, -93.281112
 
@@ -68,15 +80,17 @@ public class MapFragment extends Fragment {
         }
 
         try {
-            double latitude = 37.267510;
-            double longitude = -93.169596;
-
             for (int j = 0; j < 25; j++) {
                 List<Address> address = geocoder.getFromLocationName(loc, 1, lowerLeftLat, lowerLeftLong, upperRightLat, upperRightLong);
 
                 if (address == null || address.isEmpty()) {
                     Log.d("Error", "Latitude and longitude not found");
-                    loc = loc.substring(0, loc.lastIndexOf(' '));
+                    int index = loc.lastIndexOf(' ');
+                    if ( index == -1 ) {
+                        break;
+                    }
+
+                    loc = loc.substring(0, index);
                 } else {
                     Address destination = address.get(0);
                     latitude = destination.getLatitude();
@@ -89,7 +103,9 @@ public class MapFragment extends Fragment {
                 }
             }
 
-            googleMap.setMyLocationEnabled(true);
+            googleMap.setOnMyLocationChangeListener(myLocationChangeListener);
+
+
             MarkerOptions dest = new MarkerOptions().position(
                     new LatLng(latitude, longitude)).title("Destination");
 
@@ -109,5 +125,39 @@ public class MapFragment extends Fragment {
 
         return v;
     }
+
+    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+        DateFormat m_ISO8601Local = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
+
+        @Override
+        public void onMyLocationChange(Location location) {
+            if (location != null) {
+                float[] resultArray = new float[99];
+                Location.distanceBetween(location.getLatitude(), location.getLongitude(), latitude, longitude, resultArray);
+                try {
+                    long secToEventStart = m_ISO8601Local.parse(event.getStart()).getTime();
+                    long secToEventEnd = m_ISO8601Local.parse(event.getEnd()).getTime();
+                    long currentTime = (long) (System.currentTimeMillis() + resultArray[0]*1000);
+                    int minutes = (int) (resultArray[0]/60);
+                    int seconds = (int) (resultArray[0]%60);
+                    String timeToEvent = String.valueOf(minutes) + ":" + String.valueOf(seconds);
+
+                    if ( currentTime < secToEventStart ) {
+                        mTravelTv.setText(String.valueOf(timeToEvent));
+                        mTravelTv.setBackgroundColor(getResources().getColor(R.color.green));
+                    } else if ( currentTime < secToEventEnd ) {
+                        mTravelTv.setText(String.valueOf(timeToEvent));
+                        mTravelTv.setBackgroundColor(getResources().getColor(R.color.yellow));
+                    } else {
+                        mTravelTv.setText(String.valueOf(timeToEvent));
+                        mTravelTv.setBackgroundColor(getResources().getColor(R.color.red));
+                    }
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
 }
 
